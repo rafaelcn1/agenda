06557cfb7171253e4bfb1ebff311a44e83c4be39
Rafael Cunha <rafaelcn1@hotmail.com>
2023-09-27 14:10:25 -0300

    feat: criação PessoaDTO

---
+++ src/main/java/com/agenda/dto/PessoaDTO.java
@@ -0,0 +1,17 @@
+package com.agenda.dto;
+
+import java.util.Date;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class PessoaDTO {
+
+	private String nome;
+	private Date dataNascimento;
+
+}
