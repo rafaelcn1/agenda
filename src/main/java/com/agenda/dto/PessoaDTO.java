989094784bb9826279916810314ba0764f6285f1
Rafael Cunha <rafaelcn1@hotmail.com>
2023-09-28 14:02:15 -0300

    fix: add contatos no dto pessoa

--- src/main/java/com/agenda/dto/PessoaDTO.java
+++ src/main/java/com/agenda/dto/PessoaDTO.java
@@ -1,6 +1,10 @@
 package com.agenda.dto;
 
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
+
+import com.agenda.model.Contato;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -16,5 +20,7 @@ public class PessoaDTO {
 	private Date dataNascimento;
 	private String usuario;
 	private String senha;
+
+	private List<Contato> contatos = new ArrayList<Contato>();
 
 }
