12f7867966405f21f17314b281435347859584d4
Rafael Cunha <rafaelcn1@hotmail.com>
2023-09-28 14:03:14 -0300

    feat: criação do contato model

---
+++ src/main/java/com/agenda/model/Contato.java
@@ -0,0 +1,42 @@
+package com.agenda.model;
+
+import java.io.Serializable;
+
+import com.agenda.dto.PessoaDTO;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class Contato implements Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+	private String telefone;
+	private String email;
+
+	@ManyToOne
+	@JoinColumn(name = "pessoa_id")
+	private Pessoa pessoa;
+
+	@Override
+	public String toString() {
+		return "Contato [id=" + id + ", telefone=" + telefone + ", email=" + email + "]";
+	}
+
+}
